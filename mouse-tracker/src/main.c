776381d4d61ccf669286fad46634f6c0445e80b6
Jamie Hands <jamie_hands0@protonmail.com>
2026-01-16 17:37:52 +0000

    Added help text output when -h flag is used

--- mouse-tracker/src/main.c
+++ mouse-tracker/src/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
-int main()
+void print_help(char* exec_alias)
 {
+    printf("USAGE:\n");
+    printf("\t%s <event/file/path> [OPTIONS]\n\n", exec_alias);
+
+    printf("OPTIONS:\n");
+    printf("\t-h <help>\t\tPrints program help to the screen\n");
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2)
+    {
+        print_help(argv[0]);
+    }
+
+    if (!strcmp(argv[1], "-h"))
+    {
+        print_help(argv[0]);
+    }
 
 }
