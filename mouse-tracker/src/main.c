f38f63f543b98fc64f81f1b4f3cc75d0be31b84f
Jamie Hands <jamie_hands0@protonmail.com>
2026-01-17 17:31:10 +0000

    Added human readable key for the returned event types

--- mouse-tracker/src/main.c
+++ mouse-tracker/src/main.c
@@ -5,6 +5,43 @@
 #include <unistd.h>
 #include <linux/input.h>
 
+char* get_event_type(unsigned short type)
+{
+    switch (type)
+    {
+        case 0:
+            return "EV_SYN";
+        case 1:
+            return "EV_KEY";
+        case 2:
+            return "EV_REL";
+        case 3:
+            return "EV_ABS";
+        case 4:
+            return "EV_MSC";
+        case 5:
+            return "EV_SW";
+        case 17:
+            return "EV_LED";
+        case 18:
+            return "EV_SND";
+        case 20:
+            return "EV_REP";
+        case 21:
+            return "EV_FF";
+        case 22:
+            return "EV_PWR";
+        case 23:
+            return "EV_FF_STATUS";
+        case 31:
+            return "EV_MAX";
+        case 32:
+            return "EV_CNT";
+        default:
+            return "n/a";
+    }
+}
+
 void print_help(char* exec_alias)
 {
     printf("USAGE:\n");
@@ -37,7 +74,11 @@ int main(int argc, char* argv[])
     read(fd, &ie, sizeof(ie));
     printf("\nEvent recieved.\n");
     printf("Time = %lds %ldus\n", ie.time.tv_sec, ie.time.tv_usec);
-    printf("Type = %hu\n", ie.type);
+
+    char* type;
+    type = get_event_type(ie.type);
+    printf("Event Type = %s\n", type);
+
     printf("Code = %hu\n", ie.code);
     printf("Value = %d\n", ie.value);
 }
