7ba2c038b34f3907f23b0ce075c2de770e6176fd
Jamie Hands <jamie_hands0@protonmail.com>
2026-01-20 17:03:33 +0000

    Program now prints the address index every 16 characters, and prints the last address index at the end

--- hexdump/src/main.c
+++ hexdump/src/main.c
@@ -4,9 +4,15 @@
 int main()
 {
     int c;
+    int addr_i = 0x0;
     while((c = getchar()) != EOF)
     {
+        if (addr_i % 16 == 0)
+        {
+            printf("\n%08x ", addr_i);
+        }
         printf("%02x ", c);
+        addr_i += 1;
     }
-    printf("\n");
+    printf("\n%08x\n", addr_i);
 }
