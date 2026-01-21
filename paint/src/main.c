34b4594ecc33b2450918d40fae72fa1a3b45ad1d
Jamie Hands <jamie_hands0@protonmail.com>
2026-01-21 19:12:27 +0000

    Added window and event loop for paint program

--- paint/src/main.c
+++ paint/src/main.c
@@ -1,8 +1,43 @@
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_video.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
+
+#define WIDTH 900
+#define HEIGHT 600
 
 int main()
 {
-    return 0;
+    bool done = false;
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = SDL_CreateWindow(
+        "Paint",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        WIDTH,
+        HEIGHT,
+        0
+    );
+
+    while (!done)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                done = true;
+            }
+        }
+
+        // On every frame ...
+
+        SDL_Delay(100);
+    }
+
+    SDL_DestroyWindow(window);
 }
