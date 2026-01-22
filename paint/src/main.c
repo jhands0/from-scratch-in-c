187095b9af884c433e97a78a6c579524ec6f3813
Jamie Hands <jamie_hands0@protonmail.com>
2026-01-22 15:02:39 +0000

    Added ability to change color of paint using the scroll wheel

--- paint/src/main.c
+++ paint/src/main.c
@@ -1,5 +1,6 @@
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_mouse.h>
+#include <SDL2/SDL_stdinc.h>
 #include <SDL2/SDL_surface.h>
 #include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_video.h>
@@ -7,11 +8,20 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
+#include <wchar.h>
 
 #define WIDTH 900
 #define HEIGHT 600
 
 #define TARGET_FPS 60
+
+#define WHITE   0xFFFFFF
+#define RED     0xFF0000
+#define GREEN   0x00FF00
+#define BLUE    0x0000FF
+#define CYAN    0x00FFFF
+#define MAGENTA 0xFF00FF
+#define YELLOW  0xFFFF00
 
 int main()
 {
@@ -30,6 +40,8 @@ int main()
     SDL_Surface *surface = SDL_GetWindowSurface(window);
 
     float delay_ms = (1.0 / TARGET_FPS) * 1000;
+    int colors[7] = {WHITE, RED, GREEN, BLUE, CYAN, MAGENTA, YELLOW};
+    int color_i = 0;
     while (!done)
     {
         // On every frame ...
@@ -37,6 +49,7 @@ int main()
         bool draw;
         SDL_Rect rect;
         SDL_Event event;
+        int color;
         while (SDL_PollEvent(&event))
         {
             switch (event.type)
@@ -54,11 +67,24 @@ int main()
                     break;
 
                 case SDL_MOUSEMOTION:
+                    if (color == NULL)
+                    {
+                        color = colors[color_i];
+                    }
+
                     if (draw)
                     {
                         rect = (SDL_Rect) {event.motion.x, event.motion.y, 10, 10};
-                        SDL_FillRect(surface, &rect, 0x00FFFF);
+                        SDL_FillRect(surface, &rect, color);
                     }
+                    break;
+
+                case SDL_MOUSEWHEEL_NORMAL:
+                    color_i = (color_i + 1) % 7;
+                    break;
+
+                case SDL_MOUSEWHEEL_FLIPPED:
+                    color_i = (color_i - 1) % 7;
                     break;
             }
         }
