26a34b04ee887bf91f8d52cd66604252bec1dbb3
Jamie Hands <jamie_hands0@protonmail.com>
2026-01-26 16:47:00 +0000

    Added a draw circle function to draw the light source(s) and the object(s)

--- ray-tracing/src/main.c
+++ ray-tracing/src/main.c
@@ -4,6 +4,7 @@
 #include <SDL2/SDL_video.h>
 #include <SDL2/SDL.h>
 
+#include <math.h>
 #include <stdio.h>
 #include <stdbool.h>
 
@@ -13,12 +14,34 @@
 
 #define TARGET_FPS 60
 
+#define WHITE 0xFFFFFF
+
+void draw_circle(SDL_Surface *surface, int x, int y, int radius, int color)
+{
+    SDL_Rect pixel = (SDL_Rect) {0, 0, 1, 1};
+    for (int i = x - radius; i <= x + radius; i++)
+    {
+        for (int j = y - radius; j <= y + radius; j++)
+        {
+            int distance_from_centre = sqrt(pow(i - x, 2) + pow(j - y, 2));
+            if (distance_from_centre < radius)
+            {
+                pixel.x = i;
+                pixel.y = j;
+                SDL_FillRect(surface, &pixel, color);
+            }
+        }
+    }
+}
+
 int main()
 {
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window *window = SDL_CreateWindow("Ray Tracing", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
 
     SDL_Surface *surface = SDL_GetWindowSurface(window);
+
+    draw_circle(surface, 200, 200, 50, WHITE);
 
     float delay_ms = (1.0 / TARGET_FPS) * 1000;
     bool done = false;
