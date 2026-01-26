bff12330d1951c8ee0f1725a0391db7cfa5042c6
Jamie Hands <jamie_hands0@protonmail.com>
2026-01-26 16:38:06 +0000

    Added main event loop for the program

--- ray-tracing/src/main.c
+++ ray-tracing/src/main.c
@@ -1,8 +1,42 @@
-#include <stdio.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_surface.h>
+#include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_video.h>
 #include <SDL2/SDL.h>
 
+#include <stdio.h>
+#include <stdbool.h>
+
+
+#define WIDTH 900
+#define HEIGHT 600
+
+#define TARGET_FPS 60
 
 int main()
 {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow("Ray Tracing", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
 
+    SDL_Surface *surface = SDL_GetWindowSurface(window);
+
+    float delay_ms = (1.0 / TARGET_FPS) * 1000;
+    bool done = false;
+    while (!done)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                done = true;
+            }
+        }
+
+        SDL_UpdateWindowSurface(window);
+
+        SDL_Delay(delay_ms);
+    }
+
+    SDL_DestroyWindow(window);
 }
