27d6503c60da3d0ccffdfccd91c13c21af6be989
Jamie Hands <jamie_hands0@protonmail.com>
2026-02-09 15:35:23 +0000

    Created very basic linear heap allocator and a test case in main

--- malloc/src/main.c
+++ malloc/src/main.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+#include <assert.h>
 
 // 640 kB
 #define CAPACITY 640000
 
 char heap[CAPACITY] = {0};
+size_t heap_size = 0;
 
 void *heap_alloc(size_t size)
 {
-    return NULL;
+    assert(heap_size + size <= CAPACITY);
+    void *result = heap + heap_size;
+    heap_size += size;
+    return result;
 }
 
 void heap_free(void* ptr)
@@ -22,5 +27,10 @@ void heap_collect()
 
 int main()
 {
+    char *root = heap_alloc(26);
+    for (int i = 0; i < 26; i++)
+    {
+        root[i] = i + 'A';
+    }
     return 0;
 }
