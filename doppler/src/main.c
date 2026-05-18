13c27a2d0b3cf7a3c223495f926920d9e6da581d
Jamie Hands <jamie_hands0@protonmail.com>
2026-05-18 16:30:54 +0100

    Added some useful structs and functions

--- doppler/src/main.c
+++ doppler/src/main.c
@@ -3,18 +3,56 @@
 
 #define WIDTH 900
 #define HEIGHT 600
+#define MAX_WAVES 1000
+
+typedef struct
+{
+    float x, y
+} Vehicle;
+
+typedef struct
+{
+    float x, y, r
+} Soundwave;
+
+void propagate_waves(Soundwave* waves, unsigned int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        waves[i].r += 10;
+    }
+}
+
+void emit_new_wave(Soundwave* waves, float x, float y, unsigned int idx)
+{
+    waves[idx] = (Soundwave) {x, y, 55};
+}
 
 int main(int argc, char **argv)
 {
     InitWindow(WIDTH, HEIGHT, "Doppler Effect");
 
+    Vehicle vehicle = (Vehicle) {400, 400};
+    Soundwave waves[MAX_WAVES];
+    unsigned int current_waves = 0;
+
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
-        BeginDrawing();
+        propagate_waves(waves, current_waves);
+        emit_new_wave(waves, vehicle.x, vehicle.y, current_waves);
+        if (current_waves < MAX_WAVES) current_waves++;
 
+        BeginDrawing();
+        
         ClearBackground(BLACK);
         
+        DrawCircle(vehicle.x, vehicle.y, 50, WHITE);
+        for (int i = 0; i < current_waves; i++)
+        {
+            DrawCircleLines(waves[i].x, waves[i].y, waves[i].r, WHITE);
+        }
+
         EndDrawing();
     }
 
