340493e696088318c02145e87303a494175076b5
Jamie Hands <jamie_hands0@protonmail.com>
2026-05-18 16:57:30 +0100

    Added an interval for emitting waves and changed some constants

--- doppler/src/main.c
+++ doppler/src/main.c
@@ -4,6 +4,7 @@
 #define WIDTH 900
 #define HEIGHT 600
 #define MAX_WAVES 1000
+#define WAVE_SPEED 20
 
 typedef struct
 {
@@ -15,17 +16,17 @@ typedef struct
     float x, y, r
 } Soundwave;
 
-void propagate_waves(Soundwave* waves, unsigned int n)
+void propagate_waves(Soundwave* waves, unsigned int n, float delta)
 {
     for (int i = 0; i < n; i++)
     {
-        waves[i].r += 10;
+        waves[i].r += WAVE_SPEED * delta;
     }
 }
 
 void emit_new_wave(Soundwave* waves, float x, float y, unsigned int idx)
 {
-    waves[idx] = (Soundwave) {x, y, 55};
+    waves[idx % MAX_WAVES] = (Soundwave) {x, y, 35};
 }
 
 int main(int argc, char **argv)
@@ -35,25 +36,37 @@ int main(int argc, char **argv)
     Vehicle vehicle = (Vehicle) {400, 400};
     Soundwave waves[MAX_WAVES];
     unsigned int current_waves = 0;
+    float interval = 0;
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
-        propagate_waves(waves, current_waves);
-        emit_new_wave(waves, vehicle.x, vehicle.y, current_waves);
-        if (current_waves < MAX_WAVES) current_waves++;
+        float delta_time = GetFrameTime();
 
+        interval += delta_time;
+        if (interval > 1.0f)
+        {
+            emit_new_wave(waves, vehicle.x, vehicle.y, current_waves);
+            if (current_waves < MAX_WAVES) current_waves++;
+            interval = 0.0f;
+
+        }
+
+        propagate_waves(waves, current_waves, delta_time);
+        
         BeginDrawing();
         
         ClearBackground(BLACK);
         
-        DrawCircle(vehicle.x, vehicle.y, 50, WHITE);
+        DrawCircle(vehicle.x, vehicle.y, 20, WHITE);
         for (int i = 0; i < current_waves; i++)
         {
             DrawCircleLines(waves[i].x, waves[i].y, waves[i].r, WHITE);
         }
 
         EndDrawing();
+
+        //vehicle.x += 2;
     }
 
     return 0;
