543af9bc61750a813209c86741323bf8caa74f83
Jamie Hands <jamie_hands0@protonmail.com>
2026-05-25 18:58:57 +0100

    Added bubble sort algorithm, a way to check if an array is sorted and different colors

--- sort-viewer/src/main.c
+++ sort-viewer/src/main.c
@@ -2,12 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
 #define WIDTH 900
 #define HEIGHT 600
-#define NUM_ITEMS 50
+#define NUM_ITEMS 30
 
 int array[NUM_ITEMS];
+int idx = 0;
+bool sorted = false;
+int amount_sorted = 0;
 
 void init_array(int inverse)
 {
@@ -18,7 +22,7 @@ void init_array(int inverse)
     }
 }
 
-void draw_array(Color color)
+void draw_array()
 {
     int x_offset = 30;
     int y_offset;
@@ -26,25 +30,59 @@ void draw_array(Color color)
     int gap;
     for (int i = 0; i < NUM_ITEMS; i++)
     {
-        y_offset = array[i] * 5;
+        y_offset = array[i] * 10;
         gap = i * (width + 3);
+        Color color;
+
+        if (i == idx) color = RED;
+        else if (sorted) color = GREEN;
+        else color = LIGHTGRAY;
+
         DrawRectangle(x_offset - 5 + gap, HEIGHT * 0.8 - y_offset, width, y_offset, color);
+    }
+}
+
+void swap(int *n1, int *n2)
+{
+    int temp = *n1;
+    *n1 = *n2;
+    *n2 = temp;
+}
+
+void bubble_step()
+{
+    if (idx >= NUM_ITEMS - 1)
+    {
+        if (amount_sorted == NUM_ITEMS) sorted = true;
+        idx = 0;
+    }
+    else
+    {
+        if (array[idx] > array[idx + 1]) 
+        {
+            amount_sorted = 0;
+            swap(&array[idx], &array[idx + 1]);
+        }
+
+        idx++;
     }
 }
 
 int main(int argc, char **argv)
 {
-    int inverse;
+    bool inverse;
     if (argc == 0 || argc > 2 || argv[1] == "-h")
     {
         printf("Usage: %s <options>\n", argv[0]);
         printf("Options:\n \t-h\tHelp menu\n\t-r\tRandom order\n\t-i\tInverse order\n");
-        inverse = 0;
+        inverse = false;
     }
-    else if (argc == 1 || (argc == 2 && argv[1] == "-r")) inverse = 0;
-    else if (argc == 2 && argv[1] == "-i") inverse = 1; 
+    else if (argc == 1 || (argc == 2 && argv[1] == "-r")) inverse = false;
+    else if (argc == 2 && argv[1] == "-i") inverse = true; 
 
     InitWindow(WIDTH, HEIGHT, "Sorting Algorithm Viewer");
+
+    SetTargetFPS(60);
 
     srand(time(NULL));
 
@@ -54,9 +92,15 @@ int main(int argc, char **argv)
     {
         BeginDrawing();
 
-        draw_array(LIGHTGRAY);
+        ClearBackground(BLACK);
+
+        draw_array();
 
         EndDrawing();
+
+        bubble_step();
+
+        if (!sorted && amount_sorted != NUM_ITEMS) amount_sorted++;
     }
     
     CloseWindow();
