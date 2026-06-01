580292e367b4811b574687d0d92869483682807b
Jamie Hands <jamie_hands0@protonmail.com>
2026-06-01 15:38:42 +0100

    Added functionality to dynamically fetch address info given a port number

--- http/src/main.c
+++ http/src/main.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+
+#include <netdb.h>
+#include <netinet/in.h>
 #include <sys/socket.h>
 
 #define PORT "8080"
 
 int main()
 {
+    struct addrinfo hints, *server_info;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
 
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (getaddrinfo(NULL, PORT, &hints, &server_info) != 0)
+    {
+        perror("Could not dynamically fetch address information.\n");
+        exit(-1);
+    }
+
+    int sockfd = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
     if (sockfd < 0)
     {
         perror("Could not initialize socket.\n");
@@ -22,4 +37,6 @@ int main()
         printf("Waiting for request...\n");
         sleep(1);
     }
+
+    freeaddrinfo(server_info);
 }
