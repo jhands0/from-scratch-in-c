d1dd11ff657ae81f709a4f5dbd3fe9b4456cb792
Jamie Hands <jamie_hands0@protonmail.com>
2026-06-01 16:09:49 +0100

    Added call to listen and accept

--- http/src/main.c
+++ http/src/main.c
@@ -8,9 +8,14 @@
 #include <sys/socket.h>
 
 #define PORT "8080"
+#define BACKLOG_LEN 15
 
 int main()
 {
+    struct sockaddr_storage incoming_addr;
+    socklen_t addr_size;
+    int new_fd;
+
     struct addrinfo hints, *server_info;
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;
@@ -30,19 +35,22 @@ int main()
         exit(-1);
     }
 
-    if (bind(sockfd, server_info->ai_addr, res->ai_addrlen) == -1)
+    if (bind(sockfd, server_info->ai_addr, server_info->ai_addrlen) == -1)
     {
         perror("Could not bind socket to address.\n");
         exit(-1);
     }
     
-
-    while(1)
+    // Block until request recieved
+    if (listen(sockfd, BACKLOG_LEN) < 0)
     {
-        // Block until request recieved
-        printf("Waiting for request...\n");
-        sleep(1);
+        perror("Could not listen for connections.\n");
+        freeaddrinfo(server_info);
+        exit(-1);
     }
+
+    addr_size = sizeof(incoming_addr);
+    new_fd = accept(sockfd, (struct sockaddr *) &incoming_addr, &addr_size);
 
     freeaddrinfo(server_info);
 }
