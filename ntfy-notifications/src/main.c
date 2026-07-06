114b6be31ded7ffd18d5517777d130d7039a2740
Jamie Hands <jamie_hands0@protonmail.com>
2026-07-06 20:55:55 +0100

    Adapted sample code for custom messages and dynamic url

--- ntfy-notifications/src/main.c
+++ ntfy-notifications/src/main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 #include <curl/curl.h>
 
-int main()
+int main(int argc, char **argv)
 {
+    if (argc == 1) {
+        fprintf(stderr, "Too few arguments: \n");
+        return 0;
+    }
+
+    if (argc > 2) {
+        fprintf(stderr, "Too many arguments: \n");
+        return 0;
+    }
+
+    FILE *fptr = fopen("topic.txt", "r");
+
+    char topic[30];
+    fgets(topic, 30, fptr);
+
+    fclose(fptr);
+
+    char url[50] = "https://nfty.sh/";
+    strcat(url, topic);
+
+    const char *message = argv[1];
+    const size_t length = strlen(message);
+
     CURL *curl;
     CURLcode result;
 
@@ -16,8 +40,11 @@ int main()
     curl = curl_easy_init();
     if (curl) 
     {
-        curl_easy_setopt(curl, CURLOPT_URL, "nfty.sh/test-post-notifications");
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "name=daniel&project=curl");
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, length);
+
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, message);
 
         result = curl_easy_perform(curl);
         if (result != CURLE_OK) 
