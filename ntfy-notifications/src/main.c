d4aeb3023918337b9d0a17635d8bde89b7ec6564
Jamie Hands <jamie_hands0@protonmail.com>
2026-07-06 18:40:06 +0100

    Added sample code

--- ntfy-notifications/src/main.c
+++ ntfy-notifications/src/main.c
@@ -3,5 +3,28 @@
 
 int main()
 {
-    return 0;
+    CURL *curl;
+    CURLcode result;
+
+    result = curl_global_init(CURL_GLOBAL_ALL);
+    if (result != CURLE_OK) 
+    {
+        fprintf(stderr, "curl_global_init() failed: %s\n", curl_easy_strerror(result));
+        return (int) result;
+    }
+
+    curl = curl_easy_init();
+    if (curl) 
+    {
+        curl_easy_setopt(curl, CURLOPT_URL, "nfty.sh/test-post-notifications");
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "name=daniel&project=curl");
+
+        result = curl_easy_perform(curl);
+        if (result != CURLE_OK) 
+        {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(result));
+        }
+
+        curl_easy_cleanup(curl);
+    }
 }
