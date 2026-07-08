48638181a8dba3ddb25f4759eca884198c0cffc4
Jamie Hands <jamie_hands0@protonmail.com>
2026-07-08 15:47:09 +0100

    Added main function to setup container config from arguments, display error messages, and cleanup program

--- container/src/main.c
+++ container/src/main.c
@@ -34,3 +34,56 @@ struct child_config {
     char    **argv;
     char    *mount_dir;
 }
+
+int main(int argc, char **argv) 
+{
+    struct child_config config = {0};
+    int err = 0;
+    int option = 0;
+    int sockets[2] = {0};
+    pid_t child_pid = 0;
+    int last_optind = 0;
+
+    while ((option = getopt(argc, argv, "c:m:u:"))) {
+        switch (option) {
+        case 'c':
+            config.argc = argc - last_optind - 1;
+            config.argv = &argv[argc - config.argc];
+            goto finish_options;
+        case 'm':
+            config.mount_dir = optarg;
+            break;
+        case 'u':
+            if (sscanf(optarg, "%d", &config.uid) != 1) {
+                fprintf(stderr, "badly-formatted uid: %s\n", optarg);
+                goto usage;
+            }
+            break;
+        default:
+            goto usage;
+        }
+        last_optind = optind;
+    }
+
+finish_options:
+    if (!config.argc) goto usage;
+    if (!config.mount_dir) goto usage;
+
+    char hostname[256] = {0};
+    if (choose_hostname(hostname, sizeof(hostname)))
+        goto error;
+    config.hostname = hostname;
+
+    goto cleanup;
+
+usage:
+    fprintf(stderr, "Usage: %s -u -1 -m . -c /bin/sh -\n", argv[0]);
+
+error:
+    err = 1;
+
+cleanup:
+    if (sockets[0]) close(sockets[0]);
+    if (sockets[1]) close(sockets[1]);
+    return err;
+}
