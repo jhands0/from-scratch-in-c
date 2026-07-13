c0d6a39487b70aa845599897f147fe3498402b30
Jamie Hands <jamie_hands0@protonmail.com>
2026-07-13 17:36:59 +0100

    Added function to add a seccomp filter to the container instances

--- container/src/main.c
+++ container/src/main.c
@@ -41,6 +41,8 @@ struct child_config {
 #define USERNS_OFFSET 10000
 #define USERNS_COUNT 2000
 
+#define SCMP_FAIL SCMP_ACT_ERRNO(EPERM)
+
 
 int capabilities()
 {
@@ -150,6 +152,42 @@ int mounts(struct child_config *config)
         fprintf(stderr, "rmdir failed! %m\n");
         return -1;
     }
+    fprintf(stderr, "done.\n");
+    return 0;
+}
+
+
+int syscalls()
+{
+    scmp_filter_ctx ctx = NULL;
+    fprintf(stderr, "=> filtering syscalls...");
+    if (!(ctx = seccomp_init(SCMP_ACT_ALLOW))
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(chmod), 1, SCMP_A1(SCMP_CMP_MASKED_EQ, S_ISUID, S_ISUID))
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(chmod), 1, SCMP_A1(SCMP_CMP_MASKED_EQ, S_ISGID, S_ISGID))
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(fchmod), 1, SCMP_A1(SCMP_CMP_MASKED_EQ, S_ISUID, S_ISUID))
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(fchmod), 1, SCMP_A1(SCMP_CMP_MASKED_EQ, S_ISGID, S_ISGID))
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(fchmodat), 1, SCMP_A2(SCMP_CMP_MASKED_EQ, S_ISUID, S_ISUID))
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(fchmodat), 1, SCMP_A2(SCMP_CMP_MASKED_EQ, S_ISGID, S_ISGID))
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(unshare), 1, SCMP_A0(SCMP_CMP_MASKED_EQ, CLONE_NEWUSER, CLONE_NEWUSER))
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(clone), 1, SCMP_A0(SCMP_CMP_MASKED_EQ, CLONE_NEWUSER, CLONE_NEWUSER))
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(ioctl), 1, SCMP_A1(SCMP_CMP_MASKED_EQ, TIOCSTI, TIOCSTI))
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(keyctl), 0)
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(add_key), 0)
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(request_key), 0)
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(ptrace), 0)
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(mbind), 0)
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(migrate_pages), 0)
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(move_pages), 0)
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(set_mempolicy), 0)
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(userfaultfd), 0)
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(perf_event_open), 0)
+        || seccomp_attr_set(ctx, SCMP_FLTATR_CTL_NNP, 0)
+        || seccomp_load(ctx)) {
+            if (ctx) seccomp_release(ctx);
+            fprintf(stderr, "failed: %m\n");
+            return 1;
+    }
+    seccomp_release(ctx);
     fprintf(stderr, "done.\n");
     return 0;
 }
@@ -301,12 +339,10 @@ finish_options:
     fprintf(stderr, "%s on %s.\n", host.release, host.machine);
     // End linux validation block
 
-
     char hostname[256] = {0};
     if (choose_hostname(hostname, sizeof(hostname)))
         goto error;
     config.hostname = hostname;
-
 
     // Namespaces block
     if (socketpair(AF_LOCAL, SOCK_SEQPACKET, 0, sockets)) {
