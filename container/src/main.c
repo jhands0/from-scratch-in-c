04fa99a22ac2e054571a83ca2f712af1c8a8a0d2
Jamie Hands <jamie_hands0@protonmail.com>
2026-07-20 12:29:10 +0100

    Added structs, whose contents will be written to the containers cgroups filesystem

--- container/src/main.c
+++ container/src/main.c
@@ -193,6 +193,78 @@ int syscalls()
 }
 
 
+#define MEMORY "1073741824"
+#define SHARES "256"
+#define PIDS "64"
+#define WEIGHT "10"
+#define FD_COUNT 64
+
+struct cgrp_control {
+    char control[256];
+    struct cgrp_setting {
+        char name[256];
+        char value[256];
+    } **settings;
+};
+struct cgrp_setting add_to_tasks = {
+    .name = "tasks",
+    .value = "0"
+};
+
+struct cgrp_control *cgrps[] = {
+    & (struct cgrp_control) {
+        .control = "memory",
+        .settings = (struct cgrp_setting *[]) {
+            & (struct cgrp_setting) {
+                .name = "memory.limit_in_bytes",
+                .value = MEMORY
+            },
+            & (struct cgrp_setting) {
+                .name = "memory.kmem.limit_in_bytes",
+                .value = MEMORY
+            },
+            &add_to_tasks,
+            NULL
+        }
+    },
+    & (struct cgrp_control) {
+        .control = "cpu",
+        .settings = (struct cgrp_setting *[]) {
+            & (struct cgrp_setting) {
+                .name = "cpu.shares",
+                .value = SHARES
+            },
+            &add_to_tasks,
+            NULL
+        }
+    },
+    & (struct cgrp_control) {
+        .control = "pids",
+        .settings = (struct cgrp_setting *[]) {
+            & (struct cgrp_setting) {
+                .name = "pids.max",
+                .value = PIDS
+            },
+            &add_to_tasks,
+            NULL
+        }
+    },
+    & (struct cgrp_control) {
+        .control = "blkio",
+        .settings = (struct cgrp_setting *[]) {
+            & (struct cgrp_setting) {
+                .name = "blkio.weight",
+                .value = WEIGHT
+            },
+            &add_to_tasks,
+            NULL,
+        }
+    },
+    NULL
+};
+
+
+
 int handle_child_uid_map(pid_t child_pid, int fd) 
 {
     int uid_map = 0;
